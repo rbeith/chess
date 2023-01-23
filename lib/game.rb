86facd8b4ff7139f136694e68fae957cb158082d
Richard Beith Jr <rbeith@gmail.com>
2023-01-23 13:18:27 -0800

    Changed Main to load #load_game instead of #play_game and added #request_save to Game.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
-# Dir['/home/magnus/odinproject/ruby/ruby_projects/chess/lib/*'].each { |file| require file }
+require_relative 'save_game'
 
 class Game
+	include Save_Game
+
   attr_reader :game_board, :white, :black, :gameover
 
   def initialize(game_board = Board.new(ChessBoard.new.piece), player1 = Player.new('White'), player2 = Player.new('Black'))
@@ -18,8 +20,26 @@ class Game
     puts "#{@winner} wins!"
   end
 
+	def request_save
+		puts "\nTo save your current game enter 'save'"
+		save_game if gets.chomp == 'save'
+	end
+
+	def turn(player)
+    @game_board.move_piece(player)
+		puts @game_board.draw_board
+		request_save
+    #only need to input once? for check save.. exit.. at each step...
+
+    return unless @game_over == true
+
+    @winner = player.name
+  end
+
   def turn(player)
     @game_board.move_piece(player)
+		puts @game_board.draw_board
+		request_save
     # check_if_win
 
     return unless @game_over == true
@@ -27,16 +47,14 @@ class Game
     @winner = player.name
   end
 
-  def play_game
+  def play_game(game_board = @game_board)
     puts @game_board.draw_board
     until @game_over == true
       turn(@player1)
-      puts @game_board.draw_board
 
       break if @game_over == true
 
       turn(@player2)
-      puts @game_board.draw_board
     end
     declare_winner
   end
--- lib/main.rb
+++ lib/main.rb
@@ -2,7 +2,7 @@ require_relative 'game'
 require_relative 'board'
 require_relative 'player'
 require_relative 'ChessBoard'
+require 'yaml'
 Dir['./lib/Pieces/*'].each { |file| require file }
 
-# game = Game.new
-# game.play_game
+Game.new.load_game
