869bfeb17f7480ebe1558b004658cd4c4af06161
Richard Beith Jr <rbeith@gmail.com>
2023-02-08 15:16:35 -0800

    Fixed #path_empty not to count any final spaces.

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,45 @@
 # frozen_string_literal: true
 
 require_relative 'chess_board'
+require_relative 'check'
 
 # class for game board and movement behavior that is not piece specific
 # keeps track of piece location
 class Board
   include ChessBoard
+  include Check
 
-  attr_accessor :board
-  attr_reader :white_king, :black_king, :piece
+  attr_accessor :board, :attacker_path, :forbidden
+  attr_reader :white_king, :black_king, :piece, :checkmate
 
   def initialize(empty_space: Piece.new)
     @board = new_board
     @empty_space = empty_space
     place_pieces
+    @attacker_path = []
+    @checkmate = true
+    @forbidden = nil
   end
 
   def draw_board
     <<-BOARD
-                    1   2   3   4   5   6   7   8#{' '}
-		a | #{@board[0][0].sign} | #{@board[0][1].sign} | #{@board[0][2].sign} | #{@board[0][3].sign} | #{@board[0][4].sign} | #{@board[0][5].sign} | #{@board[0][6].sign} | #{@board[0][7].sign} |
+                    A   B   C   D   E   F   G   H#{' '}
+		8 | #{@board[7][0].sign} | #{@board[7][1].sign} | #{@board[7][2].sign} | #{@board[7][3].sign} | #{@board[7][4].sign} | #{@board[7][5].sign} | #{@board[7][6].sign} | #{@board[7][7].sign} | 8
 		  ---------------------------------
-		b | #{@board[1][0].sign} | #{@board[1][1].sign} | #{@board[1][2].sign} | #{@board[1][3].sign} | #{@board[1][4].sign} | #{@board[1][5].sign} | #{@board[1][6].sign} | #{@board[1][7].sign} |
+		7 | #{@board[6][0].sign} | #{@board[6][1].sign} | #{@board[6][2].sign} | #{@board[6][3].sign} | #{@board[6][4].sign} | #{@board[6][5].sign} | #{@board[6][6].sign} | #{@board[6][7].sign} | 7
 		  ---------------------------------
-		c | #{@board[2][0].sign} | #{@board[2][1].sign} | #{@board[2][2].sign} | #{@board[2][3].sign} | #{@board[2][4].sign} | #{@board[2][5].sign} | #{@board[2][6].sign} | #{@board[2][7].sign} |
+		6 | #{@board[5][0].sign} | #{@board[5][1].sign} | #{@board[5][2].sign} | #{@board[5][3].sign} | #{@board[5][4].sign} | #{@board[5][5].sign} | #{@board[5][6].sign} | #{@board[5][7].sign} | 6
 		  ---------------------------------
-		d | #{@board[3][0].sign} | #{@board[3][1].sign} | #{@board[3][2].sign} | #{@board[3][3].sign} | #{@board[3][4].sign} | #{@board[3][5].sign} | #{@board[3][6].sign} | #{@board[3][7].sign} |
+		5 | #{@board[4][0].sign} | #{@board[4][1].sign} | #{@board[4][2].sign} | #{@board[4][3].sign} | #{@board[4][4].sign} | #{@board[4][5].sign} | #{@board[4][6].sign} | #{@board[4][7].sign} | 5
 		  ---------------------------------
-		e | #{@board[4][0].sign} | #{@board[4][1].sign} | #{@board[4][2].sign} | #{@board[4][3].sign} | #{@board[4][4].sign} | #{@board[4][5].sign} | #{@board[4][6].sign} | #{@board[4][7].sign} |
+		4 | #{@board[3][0].sign} | #{@board[3][1].sign} | #{@board[3][2].sign} | #{@board[3][3].sign} | #{@board[3][4].sign} | #{@board[3][5].sign} | #{@board[3][6].sign} | #{@board[3][7].sign} | 4
 		  ---------------------------------
-		f | #{@board[5][0].sign} | #{@board[5][1].sign} | #{@board[5][2].sign} | #{@board[5][3].sign} | #{@board[5][4].sign} | #{@board[5][5].sign} | #{@board[5][6].sign} | #{@board[5][7].sign} |
+		3 | #{@board[2][0].sign} | #{@board[2][1].sign} | #{@board[2][2].sign} | #{@board[2][3].sign} | #{@board[2][4].sign} | #{@board[2][5].sign} | #{@board[2][6].sign} | #{@board[2][7].sign} | 3
 		  ---------------------------------
-		g | #{@board[6][0].sign} | #{@board[6][1].sign} | #{@board[6][2].sign} | #{@board[6][3].sign} | #{@board[6][4].sign} | #{@board[6][5].sign} | #{@board[6][6].sign} | #{@board[6][7].sign} |
+		2 | #{@board[1][0].sign} | #{@board[1][1].sign} | #{@board[1][2].sign} | #{@board[1][3].sign} | #{@board[1][4].sign} | #{@board[1][5].sign} | #{@board[1][6].sign} | #{@board[1][7].sign} | 2
 		  ---------------------------------
-		h | #{@board[7][0].sign} | #{@board[7][1].sign} | #{@board[7][2].sign} | #{@board[7][3].sign} | #{@board[7][4].sign} | #{@board[7][5].sign} | #{@board[7][6].sign} | #{@board[7][7].sign} |
-		  ---------------------------------
+		1 | #{@board[0][0].sign} | #{@board[0][1].sign} | #{@board[0][2].sign} | #{@board[0][3].sign} | #{@board[0][4].sign} | #{@board[0][5].sign} | #{@board[0][6].sign} | #{@board[0][7].sign} | 1
+                    A   B   C   D   E   F   G   H#{' '}
     BOARD
   end
 
@@ -54,42 +59,16 @@ class Board
     @board[start_space[0]][start_space[1]] = @empty_space
   end
 
-  def check?(piece)
-    king = if piece.color == 'white'
-             @black_king
-           else
-             @white_king
-           end
-    @board.each do |row|
-      row.each do |game_piece|
-				# FIXME: Players own piece movement reveals their king in check?
-        next unless game_piece.color != king.color &&
-                    forbidden?([game_piece.position[0], game_piece.position[1]], [king.position[0],
-                                                                                  king.position[1]], game_piece) == false
-
-        @attacker = game_piece
-        puts "\n\n#{king.color.upcase}, CHECK\n\n\n"
-				# checkmate?(space, king)
-        return true
-      end
-    end
-		false
-  end
-
-  def checkmate?(space, king)
-		# TODO: Checkmate.
-		# if no piece can block the checking path, and the king cannot move out of check. 
-		# any possible next move results in check_mate?
-		# Instance variable to capture attacking piece @attacker from #check
-		# create a Queue of spaces between @attacker and king.
-		# for each space, loop through the board checking each piece that is the same color as the king
-		# and check if there is not a #forbidden? path to the space.
-    puts 'Checkmate' 
-    # Game.end_game
-  end
+  # def king_color(piece)
+  #   if piece.color == 'white'
+  #     @black_king
+  #   else
+  #     @white_king
+  #   end
+  # end
 
   def on_board?(row, col)
-    true if row.between?(0, 7) && col.between?(0, 7)
+    @forbidden = true unless row.between?(0, 7) && col.between?(0, 7)
   end
 
   def forbidden?(from, to, piece)
@@ -97,91 +76,137 @@ class Board
     start_column = from[1]
     end_row = to[0]
     end_column = to[1]
-    return true if on_board?(end_row, end_column) == false
-    return true if piece.illegal?(@board, start_row, start_column, end_row, end_column) == true
-		return true if path_empty?(from, to) == false && piece.type != :knight
 
+    on_board?(end_row, end_column)
+    path_empty?(from, to) if piece.type != :knight
+    @forbidden = true if piece.illegal?(@board, start_row, start_column, end_row, end_column) == true
+
+    return true if @forbidden == true
+
+    @forbidden = false
     false
   end
 
   def path_empty?(from, to)
-    direction = direction(from, to)
     row = from[0]
     col = from[1]
-    case direction
-    when :up
-      (row - 1).downto(to[0]) do |i|
-        return false if @board[i][col].sign != ' '
-      end
-    when :down
-      (row + 1).upto(to[0]) do |i|
-        return false if @board[i][col].sign != ' '
-      end
-    when :left
-      (col - 1).downto(to[1]) do |j|
-        return false if @board[row][j].sign != ' '
-      end
-    when :right
-      (col + 1).upto(to[1]) do |j|
-        return false if @board[row][j].sign != ' '
-      end
-    when :up_left
-      i = row - 1
-      j = col - 1
-      while i < to[0] && j < to[1]
-        return false if @board[i][j].sign != ' '
 
-        i -= 1
-        j -= 1
-      end
-    when :up_right
-      i = row - 1
-      j = col + 1
-      while i < to[0] && j > to[1]
-        return false if @board[i][j].sign != ' '
+    if to[0] > from[0] && to[1] == from[1]
+      down(row, col, to)
+    elsif to[0] < from[0] && to[1] == from[1]
+      up(row, col, to)
+    elsif to[1] > from[1] && to[0] == from[0]
+      right(row, col, to)
+    elsif to[1] < from[1] && to[0] == from[0]
+      left(row, col, to)
+    elsif to[0] > from[0] && to[1] > from[1]
+      down_right(row, col, to)
+    elsif to[0] > from[0] && to[1] < from[1]
+      down_left(row, col, to)
+    elsif to[0] < from[0] && to[1] > from[1]
+      up_right(row, col, to)
+    elsif to[0] < from[0] && to[1] < from[1]
+      up_left(row, col, to)
+    end
+  end
 
-        i -= 1
-        j += 1
-      end
-    when :down_left
-      i = row + 1
-      j = col - 1
-      while i < to[0] && j > to[1]
-        return false if @board[i][j].sign != ' '
-
-        i += 1
-        j -= 1
-      end
-    when :down_right
-      i = row + 1
-      j = col + 1
-      while i < to[0] && j < to[1]
-        return false if @board[i][j].sign != ' '
-
-        i += 1
-        j += 1
-      end
+  def up(row, col, to)
+    (row - 1).downto(to[0] + 1) do |i|
+      arr = [] << [i, col]
+      @attacker_path = arr
+      @forbidden = true if @board[i][col].sign != ' '
+      return false if @board[i][col].sign != ' '
     end
     true
   end
 
-  def direction(from, to)
-    if to[0] > from[0] && to[1] == from[1]
-      :down
-    elsif to[0] < from[0] && to[1] == from[1]
-      :up
-    elsif to[1] > from[1] && to[0] == from[0]
-      :right
-    elsif to[1] < from[1] && to[0] == from[0]
-      :left
-    elsif to[0] > from[0] && to[1] > from[1]
-      :down_right
-    elsif to[0] > from[0] && to[1] < from[1]
-      :down_left
-    elsif to[0] < from[0] && to[1] > from[1]
-      :up_right
-    elsif to[0] < from[0] && to[1] < from[1]
-      :up_left
+  def down(row, col, to)
+    (row + 1).upto(to[0] - 1) do |i|
+      arr = [] << [i, col]
+      @attacker_path = arr
+      @forbidden = true if @board[i][col].sign != ' '
+      return false if @board[i][col].sign != ' '
     end
+    true
+  end
+
+  def left(row, col, to)
+    (col - 1).downto(to[1] + 1) do |j|
+      arr = [] << [row, j]
+      @attacker_path = arr
+      @forbidden = true if @board[row][j].sign != ' '
+      return false if @board[row][j].sign != ' '
+    end
+    true
+  end
+
+  def right(row, col, to)
+    (col + 1).upto(to[1] - 1) do |j|
+      arr = [] << [row, j]
+      @attacker_path = arr
+      @forbidden = true if @board[row][j].sign != ' '
+      return false if @board[row][j].sign != ' '
+    end
+    true
+  end
+
+  def up_left(row, col, to)
+    i = row - 1
+    j = col - 1
+    while i < to[0] - 1 && j < to[1] - 1
+      arr = [] << [i, j]
+      @attacker_path = arr
+      @forbidden = true if @board[i][j].sign != ' '
+      return false if @board[i][j].sign != ' '
+
+      i -= 1
+      j -= 1
+    end
+    true
+  end
+
+  def up_right(row, col, to)
+    i = row - 1
+    j = col + 1
+    while i < to[0] - 1 && j > to[1] + 1
+      arr = [] << [i, j]
+      @attacker_path = arr
+      @forbidden = true if @board[i][j].sign != ' '
+      return false if @board[i][j].sign != ' '
+
+      i -= 1
+      j += 1
+    end
+    true
+  end
+
+  def down_left(row, col, to)
+    i = row + 1
+    j = col - 1
+    while i < to[0] - 1 && j > to[1] + 1
+      arr = [] << [i, j]
+      @attacker_path = arr
+      @forbidden = true if @board[i][j].sign != ' '
+      return false if @board[i][j].sign != ' '
+
+      i += 1
+      j -= 1
+    end
+    true
+  end
+
+  def down_right(row, col, to)
+    i = row + 1
+    j = col + 1
+    while i < to[0] - 1 && j < to[1] - 1
+      arr = [] << [i, j]
+      @attacker_path = arr
+      @forbidden = true if @board[i][j].sign != ' '
+      return false if @board[i][j].sign != ' '
+
+      i += 1
+      j += 1
+    end
+    true
   end
 end
