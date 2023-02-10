0e8985b683ea70f788a2877f2974d25cf539c296
Richard Beith Jr <rbeith@gmail.com>
2023-02-09 22:22:18 -0800

    Added checks to #verify_input to forbid move to empty space or same color space.

--- lib/game.rb
+++ lib/game.rb
@@ -9,8 +9,8 @@ class Game
   attr_reader :game_board, :white, :black, :gameover, :current_player
 
   def initialize(game_board: Board.new,
-                 player1: Player.new(name: 'White'),
-                 player2: Player.new(name: 'Black'))
+                 player1: Player.new(name: 'White', color: 'white'),
+                 player2: Player.new(name: 'Black', color: 'black'))
     @game_board = game_board
     @player1 = player1
     @player2 = player2
@@ -41,17 +41,6 @@ class Game
     end
   end
 
-  def player_input
-    puts "#{@current_player.name}, select a piece to move; enter column then row. eg: 'b1'"
-    puts "Or enter 'save' or 'quit'"
-    save_or_quit(select_piece)
-    @current_player.translate_piece_selection
-    puts 'Select the space to move your piece to'
-    save_or_quit(select_space)
-    @current_player.translate_space_selection
-    player_input if verify_input(@current_player, current_piece, selected_space, selected_piece) == false
-  end
-
   def select_piece
     @current_player.select_piece
   end
@@ -65,7 +54,7 @@ class Game
   end
 
   def current_piece
-    @current_piece = @game_board.piece[@current_player.piece[0]][@current_player.piece[1]]
+    @current_piece = @game_board.board[@current_player.piece[0]][@current_player.piece[1]]
   end
 
   def reset_space(space)
@@ -84,13 +73,34 @@ class Game
     @current_player.piece
   end
 
-  def verify_input(_player, start_piece, end_space, start_space)
-    if @game_board.forbidden?(start_space, end_space, start_piece) == true
-      puts "\n\nForbidden move, choose again.\n\n\n"
-      false
-    else
-      true
-    end
+  def player_input
+    # TODO: no error for invalid input
+    # TODO: Highlight or announce or display selected piece
+    puts "#{@current_player.name}, select a piece to move; enter column then row. eg: 'b1'"
+    puts "Or enter 'save' or 'quit'"
+    save_or_quit(select_piece)
+    @current_player.translate_piece_selection
+    puts 'Select the space to move your piece to'
+    save_or_quit(select_space)
+    @current_player.translate_space_selection
+    player_input if verify_input(@current_player, current_piece, selected_space, selected_piece) == false
+  end
+
+	def same_color?(player, end_piece)
+		true if player.color == end_piece.color
+	end
+
+	def space_not_empty?(end_piece)
+		true if end_piece.sign == ' '
+	end
+
+  def verify_input(player, start_piece, end_space, start_space)
+	  end_piece = @game_board.board[end_space[0]][end_space[1]]
+		verified = true
+		verified = false if same_color?(player, end_piece) || space_not_empty?(end_piece) ||
+    @game_board.forbidden?(start_space, end_space, start_piece)
+    puts "\n\nForbidden move, choose again.\n\n\n" if verified == false
+    verified
   end
 
   def turn
@@ -107,7 +117,7 @@ class Game
     @winner = @current_player.name
   end
 
-  def play_game(_game_board = @game_board)
+  def play_game
     puts draw_board
     until @game_over == true
       @current_player = @player1
