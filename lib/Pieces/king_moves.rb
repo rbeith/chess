ca1535942f1a7eb70b7c9558fd8dff2bbe093a3a
Richard Beith Jr <rbeith@gmail.com>
2023-02-09 22:21:06 -0800

    Added variable to store array of all possible king moves from current space.

--- lib/Pieces/king_moves.rb
+++ lib/Pieces/king_moves.rb
@@ -2,6 +2,10 @@
 
 # Movement conditions for Kings
 module Kingmoves
+	def possible_moves
+    possible_moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+	end
+
   def illegal?(_board, start_row, start_column, end_row, end_column)
     case [end_row, end_column]
     when [start_row + 1, start_column]
